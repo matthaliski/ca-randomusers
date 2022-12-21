7a14ecabed64d0120c77ad8ea992d47d8ce1b6fb
Matt Haliski <matt@haliski.com>
2022-12-21 11:03:22 -0800

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Matt Haliski on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
